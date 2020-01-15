9ee765e8d111f67492e4554789eef26331a7037d
Adrian Corsini <glenflesk@gmail.com>
2020-01-15 22:03:29 +0000

    Defining what a template pattern is

---
+++ template_method_pattern.py
@@ -0,0 +1,4 @@
+#Template method is a behavioral design
+# pattern that defines the skeleton of an algorithm
+# in the base class but lets derived classes overide specific
+# steps of the algorithm without changing its structure
