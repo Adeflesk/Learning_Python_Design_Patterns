0b6018dd0d35fafa851d292fe92463bc0351f629
Adrian Corsini <glenflesk@gmail.com>
2020-01-20 21:07:20 +0000

    Practised the adapter pattern with the file poultry.py

--- Adapter_pattern.py
+++ Adapter_pattern.py
@@ -2,3 +2,23 @@
 #interface of a class into another interface clients
 #expect. Adapter lets classes work together that couldn't otherwise
 #because of incompatible interfaces.
+from template_method_pattern import FileAverageCalculator
+from random import randint
+
+class GeneratorAdapter:
+
+    def __init__(self, adaptee):
+        self.adaptee = adaptee
+
+    def readline(self):
+        try:
+            return next(self.adaptee)
+        except StopIteration:
+            return ''
+    
+    def close(self):
+        pass
+
+g = (randint(1, 100)for i in range(1000000))
+fac = FileAverageCalculator(GeneratorAdapter(g))
+print(fac.average())
