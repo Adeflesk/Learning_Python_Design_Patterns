d849e23d7777d5a4f46ff968aa43e144b541012f
Adrian Corsini <glenflesk@gmail.com>
2020-01-20 21:11:41 +0000

    Created observer_pattern.py and wrote description

---
+++ observer_pattern.py
@@ -0,0 +1,4 @@
+#Observer is a behavioral desigh pattern
+#that defines a one-to-many dependency between
+#between objects so that when one object changes state
+#all its dependents are notified and update automatically
